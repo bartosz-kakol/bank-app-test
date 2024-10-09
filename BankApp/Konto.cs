3dc20e46ecdc9ac4e068646f1b8fcd9ef7f44a8a
bartekmuzyk <bartosz.kakol@proton.me>
2024-10-09 13:19:39 +0200

    Uzupełnienie klasy Konto i testy

--- BankApp/Konto.cs
+++ BankApp/Konto.cs
@@ -2,8 +2,16 @@
 {
     public class Konto
     {
-        public Konto()
+        public string Imie;
+
+        public string Nazwisko;
+
+        public int Saldo = 0;
+
+        public Konto(string imie, string nazwisko)
         {
+            Imie = imie;
+            Nazwisko = nazwisko;
         }
     }
 }
