20db4ec88476a429f70f36c0467c2a4b6aa8e3df
bartekmuzyk <bartosz.kakol@proton.me>
2024-10-09 13:05:08 +0200

    Dodaj pliki projektów.

---
+++ BankApp/Konto.cs
@@ -0,0 +1,7 @@
+﻿namespace BankApp
+{
+    public class Konto
+    {
+
+    }
+}
---
+++ Testy/TestCreateBankAccount.cs
@@ -0,0 +1,16 @@
+namespace Testy
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
