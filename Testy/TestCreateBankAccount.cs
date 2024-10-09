1c6b5a4026fae60495a8c111ec04ca2cbdb22bf5
bartekmuzyk <bartosz.kakol@proton.me>
2024-10-09 13:14:27 +0200

    Pierwszy test

--- Testy/TestCreateBankAccount.cs
+++ Testy/TestCreateBankAccount.cs
@@ -1,10 +1,15 @@
+using BankApp;
+
 namespace Testy
 {
     public class Tests
     {
+        private Konto konto;
+
         [SetUp]
         public void Setup()
         {
+            konto = new Konto();
         }
 
         [Test]
